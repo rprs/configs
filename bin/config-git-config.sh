bb8ffad36459fc7d1aa92de284a657920a77f684
roberto <robertoprs@gmail.com>
2018-04-26 00:51:45 -0700

    Fix git nothave to really exclude files.

--- bin/config-git-config.sh
+++ bin/config-git-config.sh
@@ -4,7 +4,7 @@ git config --global difftool.prompt false
 git config --global alias.d difftool
 
 # List files not tracked.
-git config --global alias.nothave "ls-files . --ignored --exclude-standard"
+git config --global alias.nothave "ls-files . --ignored --exclude-standard --others"
 # pretty log with relative dates and authors.
 git config --global alias.l "log --date=short-local --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) %C(bold green)(%ad)%C(reset) %C(white)%s%C(reset) %C(bold yellow)%d%C(reset)' --all"
 git config --global alias.ll "log --graph --date=short-local --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) %C(bold green)(%ad)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
