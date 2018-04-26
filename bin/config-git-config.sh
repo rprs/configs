cd7ff360d31a849fcbc8001bef2f68c8cde18d30
roberto <robertoprs@gmail.com>
2018-04-26 00:37:10 -0700

    Add git alias for listing ignored files (nothave)

--- bin/config-git-config.sh
+++ bin/config-git-config.sh
@@ -3,6 +3,8 @@ git config --global diff.tool vimdiff
 git config --global difftool.prompt false
 git config --global alias.d difftool
 
+# List files not tracked.
+git config --global alias.nothave "ls-files . --ignored --exclude-standard"
 # pretty log with relative dates and authors.
 git config --global alias.l "log --date=short-local --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) %C(bold green)(%ad)%C(reset) %C(white)%s%C(reset) %C(bold yellow)%d%C(reset)' --all"
 git config --global alias.ll "log --graph --date=short-local --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) %C(bold green)(%ad)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
