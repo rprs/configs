3a332e0aec0d15d9bd36b92f0285020426431e20
roberto <robertoprs@gmail.com>
2018-04-27 14:59:11 -0700

    Add pycscope to progreams to install.

--- bin/ubuntu-install-programs.sh
+++ bin/ubuntu-install-programs.sh
@@ -1,7 +1,9 @@
 sudo apt install vlc
 sudo apt install feh
 sudo apt install python-bs4
-sudo apt install ag
 sudo apt install silversearcher-ag
 sudo apt install fzf
 sudo apt install cscope
+sudo apt install python-pip
+pip install pycscope
+pip install --upgrade pip
