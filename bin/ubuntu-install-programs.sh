2397268a901f6d8e9dc80bc77469c43b668a6103
roberto <robertoprs@gmail.com>
2018-05-01 21:20:41 -0700

    Add instructions to install YouCompleteMe
    - Add list of programs that I need to install to compile YCM correctly.
    - Also adding comments to explain what the programs are used for.

--- bin/ubuntu-install-programs.sh
+++ bin/ubuntu-install-programs.sh
@@ -1,9 +1,22 @@
+# General use
 sudo apt install vlc
 sudo apt install feh
 sudo apt install python-bs4
+
+# For vim plugins.
 sudo apt install silversearcher-ag
 sudo apt install fzf
 sudo apt install cscope
 sudo apt install python-pip
 pip install pycscope
 pip install --upgrade pip
+
+# Vim YouCompleteMe plugin dependencies
+sudo apt install build-essential cmake
+sudo apt install python-dev python3-dev
+sudo apt install mono-xbuild
+sudo apt install golang-go
+
+# Instaling YouCompleteMe plugin
+cd ~/.vim/vim-addons/github-Valloric-YouCompleteMe/
+./install.py --clang-completer --cs-completer --go-completer --java-completer
