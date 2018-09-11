1d70f1fad6cc5185827ba0287e5576b0e3d5a21c
roberto <robertoprs@gmail.com>
2018-09-10 23:49:28 -0700

    Fix destination paths for install-swift-vim.
    Adding full path to destination folder so the script can be run from
    anywhere.

--- bin/install-swift-syntax-for-vim.sh
+++ bin/install-swift-syntax-for-vim.sh
@@ -5,8 +5,8 @@ mkdir swift-tmp
 git clone https://github.com/apple/swift.git swift-tmp/
 
 # Copy the vim files.
-cp -r swift-tmp/utils/vim/ftdetect .vim/
-cp -r swift-tmp/utils/vim/ftplugin .vim/
+cp -r swift-tmp/utils/vim/ftdetect ~/.vim/
+cp -r swift-tmp/utils/vim/ftplugin ~/.vim/
 cp -r  swift-tmp/utils/vim/syntax ~/.vim/
 
 # Remove apple swift repo. We got what we wanted.
