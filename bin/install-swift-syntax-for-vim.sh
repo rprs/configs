4b8dabfc97b32c67f2d5a35abc21e2dfeb023117
roberto <robertoprs@gmail.com>
2018-09-10 23:46:42 -0700

    Add script to get swift vim tools from Apple repo.
    Gets the utils/vim directories from apple swift github repo into ~/.vim
    so that vim has syntax coloring for swift files.

---
+++ bin/install-swift-syntax-for-vim.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Copy apple swift repo from github.
+mkdir swift-tmp
+git clone https://github.com/apple/swift.git swift-tmp/
+
+# Copy the vim files.
+cp -r swift-tmp/utils/vim/ftdetect .vim/
+cp -r swift-tmp/utils/vim/ftplugin .vim/
+cp -r  swift-tmp/utils/vim/syntax ~/.vim/
+
+# Remove apple swift repo. We got what we wanted.
+rm -rf swift-tmp/
