fb5919c5c32a5bab0efaace5d423cd4272e44939
roberto <robertoprs@gmail.com>
2020-03-01 16:33:17 -0800

    Swift install: Only download required files instead of whole repo.

--- bin/install-swift-syntax-for-vim.sh
+++ bin/install-swift-syntax-for-vim.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# Copy apple swift repo from github.
-mkdir swift-tmp
-git clone https://github.com/apple/swift.git swift-tmp/
+# This script grabs the vim files in the github repo for Swift:
+# https://github.com/apple/swift.git
+# There is a ftplugin file, but we don't seem to need it.
 
-# Copy the vim files.
-cp -r swift-tmp/utils/vim/ftdetect ~/.vim/
-cp -r swift-tmp/utils/vim/ftplugin ~/.vim/
-cp -r  swift-tmp/utils/vim/syntax ~/.vim/
+# Creates the directories where we will put the files.
+mkdir ~/.vim/syntax
+mkdir ~/.vim/ftdetect
 
-# Remove apple swift repo. We got what we wanted.
-rm -rf swift-tmp/
+# Copy ftdetect files.
+curl https://raw.githubusercontent.com/apple/swift/master/utils/vim/ftdetect/sil.vim > ~/.vim/ftdetect/sil.vim
+curl https://raw.githubusercontent.com/apple/swift/master/utils/vim/ftdetect/swift.vim > ~/.vim/ftdetect/swift.vim
+curl https://raw.githubusercontent.com/apple/swift/master/utils/vim/ftdetect/swiftgyb.vim > ~/.vim/ftdetect/swiftgyb.vim
+
+# Copy syntax files.
+curl https://raw.githubusercontent.com/apple/swift/master/utils/vim/syntax/sil.vim > ~/.vim/syntax/sil.vim
+curl https://raw.githubusercontent.com/apple/swift/master/utils/vim/syntax/swift.vim > ~/.vim/syntax/swift.vim
+curl https://raw.githubusercontent.com/apple/swift/master/utils/vim/syntax/swiftgyb.vim > ~/.vim/syntax/swiftgyb.vim
